92a51b5995c856ab20b2715f924b4cf14b2beeaa
Braian Alberto Vargas <braianvargas1616@gmail.com>
2020-11-21 21:11:52 -0300

    Agregar archivos de proyecto.

---
+++ AppTaller/Classes/Vehiculos.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppTaller
+{
+    class Vehiculos
+    {
+        private string estado;
+        private string patente;
+        private string nombreDuenio;
+        private DateTime fechaIngreso;
+        private float precio;
+        public Vehiculos(string state, string pat, string duenio, DateTime fec, float price)
+        {
+            estado = state;
+            patente = pat;
+            nombreDuenio = duenio;
+            fechaIngreso = fec;
+            precio = price;
+        }
+        public string getEstado() { return estado; }
+        public string getPatente() { return patente; }
+        public string getDuenio() { return nombreDuenio; }
+        public DateTime getFecha() { return fechaIngreso; }
+        public float getPrecio() { return precio; }
+        
+    }
+}
